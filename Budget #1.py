4af55fdfc83b8390a44e2be892381925dda2a81a
mtreynolds55 <54384007+mtreynolds55@users.noreply.github.com>
2019-11-12 22:28:52 -0500

    Add files via upload

---
+++ Budget #1.py
@@ -0,0 +1,19 @@
+import os
+import sys
+class Application:
+    def __init__(self):
+        self.income = 0
+        self.expenses = 0
+        self.expense_list = []
+        self.expense_name = []
+        self.income_name = []
+        self.income_list = []
+        self.prompt_income()
+
+        
+
+
+	
+
+	
+
